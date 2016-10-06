40d80a2278f8da3a04d933e1803ed1d91f3e1a4d
Jason Hammond <jasonhammond84@gmail.com>
2016-10-06 13:22:03 -0500

    Another JS attempt

--- www/js/index.js
+++ www/js/index.js
@@ -1,32 +1,11 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-document.addEventListener("deviceready", onDeviceReady, false);
-
-function onDeviceReady() {
-console.log('device ready');
+$(document).on('pageinit', '#frontpage', function() {
+    console.log('device ready');
 
     var cardToggle;
     var card;
     var cards;
     
-    $('#frontpage').bind('pageshow', function(event) {
+    $('#frontpage').bind('pageload pageshow', function(event) {
         cardToggle = 0;
         cardDraw();
     });
@@ -57,19 +36,19 @@ console.log('device ready');
     
         card = (cards[Math.floor(Math.random() * cards.length)]);
     };
-    
-//    $('#frontpage').ready(function() {
-        $('#qa').bind('tap', function() {        
-            console.log('tap bind OK'); 
-            if ((cardToggle % 2) === 0) {
-                $(this).html(card.question);
-                cardToggle += 1;
-            }
-            else {
-                $(this).html(card.answer);
-                cardDraw();
-                cardToggle += 1;
-            }
-        });
-//    });
-};
+});    
+
+$('#frontpage').ready(function() {
+    $('#qa').bind('tap', function() {        
+        console.log('tap bind OK'); 
+        if ((cardToggle % 2) === 0) {
+            $(this).html(card.question);
+            cardToggle += 1;
+        }
+        else {
+            $(this).html(card.answer);
+            cardDraw();
+            cardToggle += 1;
+        }
+    });
+});
